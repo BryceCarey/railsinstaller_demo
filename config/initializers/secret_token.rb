950d16374fc601f4d52118d7976e97bc344fd8fc
Bryce Carey <brycey@gmail.com>
2012-06-21 00:37:54 -0700

    Github is odd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '0807fbccef5fed3987b5aef264fd44efcd760b750f8ead0e1a935b0cfc29c7379dd827022142638e2b6d8b97d7efc06b4fbc3dc9347ab763bb9251f8c0357150'
